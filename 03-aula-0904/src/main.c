3c5971a78d5dd6380febe4350b22273f56b69002
barbier1 <162060319+pedro-barbier@users.noreply.github.com>
2026-04-09 23:17:53 +0000

    aula dia 09 de abril

---
+++ 03-aula-0904/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Aula 03, dia 09 de abril de 2026/1, sobre String, Structs e apresentação do trabalho 1
+int main() {
+    printf("Olá do projeto %s!\n", "03-aula-0904");
+    return 0;
+}
